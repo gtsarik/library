eef79e2cd3d0cee59e694caa176f88e248e8d9fa
Grigoriy Tsarik <artgrem@gmail.com>
2015-02-03 14:12:58 +0200

    fix config file

--- config.py
+++ config.py
@@ -6,5 +6,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'library_user_request_safe'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'library.db')
+# SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'library.db')
+
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'library.db')
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+    
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
