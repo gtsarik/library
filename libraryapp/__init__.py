ba66963c55c611eebd7a06513cea57ad35d52675
Grigoriy Tsarik <artgrem@gmail.com>
2015-02-03 12:55:16 +0200

    fix logging in __init__.py

--- libraryapp/__init__.py
+++ libraryapp/__init__.py
@@ -18,3 +18,22 @@ login_manager.login_view = 'login'
 
 from libraryapp.views import viewapp
 from libraryapp.models import modelapp
+
+
+
+if not app.debug and os.environ.get('HEROKU') is None:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('tmp/microblog.log', 'a', 1 * 1024 * 1024, 10)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    app.logger.addHandler(file_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
+
+if os.environ.get('HEROKU') is not None:
+    import logging
+    stream_handler = logging.StreamHandler()
+    app.logger.addHandler(stream_handler)
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('microblog startup')
